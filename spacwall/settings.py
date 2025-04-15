2f130e6ac0034b0040f70187e8114651bd9a6d71
GJablo <gibbyjablo254@gmail.com>
2025-04-15 11:56:26 +0300

    edited settings py file

--- spacwall/settings.py
+++ spacwall/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*1f##c-8thn-6c$f2(-34=1#=bgy-)l9&ocqz)t&g(gmxbaf)d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spacnwal.co.ke', 'www.spacnwal.co.ke']
 
 
 # Application definition
